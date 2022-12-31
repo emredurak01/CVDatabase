bf1ce888948e4b53a2fc815135367f7f8dc3bb1b
emredurak <emredurak01@gmail.com>
2022-12-31 18:34:53 +0300

    Deletable attributes and Help UI

---
+++ src/main/java/com/example/cvdatabase/Controller/AddControllers/HelpController.java
@@ -0,0 +1,29 @@
+package com.example.cvdatabase.Controller.AddControllers;
+
+
+import io.github.palexdev.materialfx.dialogs.MFXGenericDialog;
+import io.github.palexdev.materialfx.font.MFXFontIcon;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelpController implements Initializable {
+    @FXML
+    private MFXGenericDialog rootPane;
+    @FXML
+    private MFXFontIcon minimizeIcon;
+    @FXML
+    private MFXFontIcon closeIcon;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        closeIcon.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> ((Node) (event.getSource())).getScene().getWindow().hide());
+        minimizeIcon.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> ((Stage) rootPane.getScene().getWindow()).setIconified(true));
+    }
+}
